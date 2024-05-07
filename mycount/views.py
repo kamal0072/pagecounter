9e14d282e0703267e8f6226324f136226b18e743
Haaris <haaris@trainingbasket.in>
2024-05-07 17:02:47 +0530

    Page counter in django

---
+++ mycount/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    ct = request.session.get('count', 0)
+    newCount = ct+1
+    request.session['count'] = newCount
+    return render(request, 'home.html', {'count' : newCount})
